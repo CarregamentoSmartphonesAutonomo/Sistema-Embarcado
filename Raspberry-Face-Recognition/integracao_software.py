f27a317dfb8566fc196d34215f5b79a2a95155a2
Lucas Rufino <lucaslrt@outlook.com>
2018-05-26 17:42:14 -0300

    Changing server socket logic.

--- Raspberry-Face-Recognition/integracao_software.py
+++ Raspberry-Face-Recognition/integracao_software.py
@@ -9,12 +9,27 @@ s.bind((host, port))
 s.listen(5)
 while True:
   c, addr = s.accept()
-  print ('Got connection from',addr)
-  c.send('Thank you for connecting')
 
-  if c == 1:
-    remove_smartphone()
-  elif c == 2:
-    c.send('Running functionality 2')
-  else:
-    c.close()
+   try:
+        print ('Got connection from',addr)
+        # Receive the commands data in small chunks and retransmit the results
+        while True:
+            data = connection.recv(16)
+            print ('received "%s"' % data)
+            if data:
+                print ('go to step "%s"' % data)
+
+                if c == 1:
+                  result = remove_smartphone()
+                  connection.sendall(result)
+                elif c == 2:
+                  c.send('Running functionality 2')
+                else:
+                  c.close()
+            else:
+                print >>sys.stderr, 'no more data from', client_address
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
